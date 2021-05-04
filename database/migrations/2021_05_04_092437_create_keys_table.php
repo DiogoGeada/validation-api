b716ca072c744f6686690b1b871fb6f329767b37
Diogo <diogocgeada@gmail.com>
2021-05-04 11:08:41 +0100

    Added user model; Created users table with migration

--- database/migrations/2021_05_04_092437_create_keys_table.php
+++ database/migrations/2021_05_04_092437_create_keys_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateKeysTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('keys', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('activation_key')->length(255)->unique()->nullable(false);
             $table->timestamps();
+            $table->datetime('activation_date')->default(null);
+            $table->foreignId('user_id')->references('id')->on('users');
         });
     }
 
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('keys');
     }
 }
